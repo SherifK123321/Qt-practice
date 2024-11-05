0c3c737ebfa6ed5a9b571282444ee7ce38c85960
Sherifk123321 <kirillserifov@gmail.com>
2024-11-05 19:38:00 +0700

    first commit

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_untitled_FILE D:/Priject/untitled/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/untitled.exe)
+set(__QT_DEPLOY_TARGET_untitled_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_untitled_RUNTIME_DLLS D:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
